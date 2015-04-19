a60b3d7bfdfd37f5a06cfd2675fed9f0b58a6214
Michael Alessandro de Souza <michael.asst@gmail.com>
2015-04-19 20:15:36 -0300

    Pagina inicial
    Inserir a view no mainpagedao

--- Controller/master-controller (2).php
+++ Controller/master-controller (2).php
@@ -2,18 +2,23 @@
 	require_once "..\model\class-usuariodao.php";
 	class MasterController{
 		
-		private $usuariodao = new UsuarioDao();
+		private $usuariodao;
 		
-		public function listarGrupos($login = $_SESSION['login']){
+		public function listarGrupos($login){
 			foreach($this->usuariodao->consultarGrupos($login) as $nome){
 				echo $nome;
 			}
 		}
 		
-		public function listarDisciplinas($login = $_SESSION['login']){
+		public function listarDisciplinas($login){
 			foreach($this->usuariodao->consultarDisciplinas($login) as $nome){
 				echo $nome;
 			}
 		}
+                
+                public function __construct(){
+                    $this->usuariodao = new Usuario();
+                }
+                       
 	}
 ?>
