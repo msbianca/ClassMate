f37f6a145533f40f5377c5905b02cc786bb485a9
Michael Alessandro de Souza <michael.asst@gmail.com>
2015-04-26 19:04:43 -0300

    Inserir e listar pastas e arquivos

--- Model/class-arquivodao.php
+++ Model/class-arquivodao.php
@@ -1,42 +1,39 @@
 <?php
-	class ArquivoDao
-	{
-	public function inserirArquivo($idPasta, $idParticipacao,$nome, $descricao, $caminho){
-		$query = "INSERT INTO tbArquivo (idPasta, idParticipacao, nome, descricao, caminhoArquivo)  VALUES ( '".$idPasta."', '".$idParticipacao."', '".$nome."', '".$descricao."', '".$caminho."')";
-		
-				mysql_query($query);
-		
-	}
 
-	public function alterarArquivo($idArquivo, $idPasta, $idParticipacao,$nome, $descricao, $caminho){
-		$query = "UPDATE tbArquivo SET idPasta = $idPasta AND idParticipacao = $idParticipacao AND nome = $nome AND descricao = $descricao AND  caminhoArquivo = $caminho  WHERE idArquivo = $idArquivo";
-		
-				mysql_query($query);
-			
-		}
+class ArquivoDao {
 
-	public function removerArquivo($idArquivo){
-		$query = "DELETE  FROM tbArquivo WHERE idArquivo = $idArquivo";
-		mysql_query($query);
-		
-		}
+    public function inserirArquivo($idPasta, $idParticipacao, $nome, $descricao, $caminho) {
+        $query = "INSERT INTO tbArquivo (idPasta, idParticipacao, nome, descricao, caminhoArquivo)  VALUES ( '" . $idPasta . "', '" . $idParticipacao . "', '" . $nome . "', '" . $descricao . "', '" . $caminho . "')";
 
-	public function listarTodosArquivos($idParticipacao){
-		$query = "SELECT * FROM tbArquivo WHERE idParticipacao = $idParticipacao";
-		mysql_query($query);
-		
-	}
-	public function listarArquivos($idArquivo){
-		$query = "SELECT * FROM tbArquivo WHERE idArquivo = $idArquivo";
-		mysql_query($query);
-		
-	}
+        mysql_query($query);
+    }
 
+    public function alterarArquivo($idArquivo, $idPasta, $idParticipacao, $nome, $descricao, $caminho) {
+        $query = "UPDATE tbArquivo SET idPasta = $idPasta AND idParticipacao = $idParticipacao AND nome = $nome AND descricao = $descricao AND  caminhoArquivo = $caminho  WHERE idArquivo = $idArquivo";
 
+        mysql_query($query);
+    }
 
-		
+    public function removerArquivo($idArquivo) {
+        $query = "DELETE  FROM tbArquivo WHERE idArquivo = $idArquivo";
+        mysql_query($query);
+    }
 
-		
-	}
-	
+    public function listarTodosArquivos($idParticipacao) {
+        $query = "SELECT * FROM tbArquivo WHERE idParticipacao = $idParticipacao";
+        mysql_query($query);
+    }
+
+    public function listarArquivos($idArquivo) {
+        $query = "SELECT * FROM tbArquivo WHERE idArquivo = $idArquivo";
+        $stmt = mysql_query($query);
+        $result = array();
+        while ($rs = mysql_fetch_array($stmt)) {
+            array_push($result, $rs);
+        }
+        return $result;
+    }
+
+}
+
 ?>	
