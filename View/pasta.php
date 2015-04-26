a712c5a20a7d65036e60caf8bb4e881f4983418f
Bianca Martins <bia.mart05@gmail.com>
2015-04-26 17:14:30 -0300

    ArquivosDao

---
+++ View/pasta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>ClassMate</title>
+        <link href="assets/estilo.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="assets/css/tabs.css" rel="stylesheet" type="text/css" media="all" />
+    </head>
+
+    <body>
+
+        <div id="principal">
+
+            <?php
+            include("master.php");
+            ?>
+             <center>
+                <div id="divConteudo">
+                    <?php
+                         include('../Controller/montararquivos-controller.php');
+                  
+                    ?>
+
+                </div>
+            </center>
+
+
+        </div>
+
+    </body>
+</html>
